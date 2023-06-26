86f4eecbfb775263e2f4651a7d2093cb40fe4dd6
Константин <polyakov_ks@smart-transport.ru>
2023-06-26 09:34:18 +0500

    Initial page draft

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
 @app.route('/faq')
-def hello_world():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
 
 
 # @app.route('/contacts')   TODO Сделать страницу с контактами
