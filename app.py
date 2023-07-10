ef6211040c6ba14000b9bc578b79bfdbb9b9ada5
Константин <polyakov_ks@smart-transport.ru>
2023-07-10 18:33:04 +0500

    add access and refresh token

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from flask import Flask, render_template, request
-from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
+from flask import Flask, render_template, request, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_refresh_token
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 
 from config import Config
-from models import Users
-
+from models import session, Users
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
 client = app.test_client()
 
-Base = declarative_base()
-
 engine = create_engine('postgresql+psycopg2://scot:tiger@localhost:5432/mydatabase')
-
-session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-
 
 jwt = JWTManager(app)
 
@@ -29,6 +25,7 @@ def index():
 
 
 @app.route('/about')
+@jwt_required()
 def about():
     return render_template('about.html')
 
@@ -39,16 +36,20 @@ def register():
     user = Users(**params)
     session.add(user)
     session.commit()
-    token = user.get_token()
-    return {'access_token': token}
+    access_token = user.get_token()
+    return jsonify({'access_token': access_token})
 
 
 @app.route('/login', methods=['POST'])
 def login():
     params = request.json
     user = Users.authenticate(**params)
-    token = user.get_token()
-    return {'access_token': token}
+    access_token = user.get_token()
+    refresh_token = create_refresh_token(user.id)
+    return jsonify({
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+    })
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,51 +1,59 @@
 from datetime import timedelta
 
+from bcrypt import checkpw
 from flask_jwt_extended import create_access_token
 from passlib.hash import bcrypt
 from sqlalchemy import create_engine, DateTime, ForeignKey, func, Integer, String
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-
-
-Base = declarative_base()
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, scoped_session, sessionmaker
+from werkzeug.exceptions import Unauthorized
 
 engine = create_engine('postgresql+psycopg2://scot:tiger@localhost:5432/mydatabase')
 
+Base = declarative_base()
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
 
 class Users(Base):
-    """Пользователь"""
-
+    """
+    Пользователь
+    """
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
     name: Mapped[str | None] = mapped_column(String(80))
-    password_hash: Mapped[str] = mapped_column(String(120))
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
     time_created: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
     def __init__(self, **kwargs):
         self.username = kwargs.get('username')
         self.name = kwargs.get('name')
-        self.password_hash = bcrypt.hash(kwargs.get('password_hash'))
+        self.password = bcrypt.hash(kwargs.get('password'))
         self.time_created = kwargs.get('time_created')
         self.email = kwargs.get('email')
 
-    def get_token(self, expire_time=24):
+    def get_token(self, expire_time=1):
         expire_delta = timedelta(expire_time)
         token = create_access_token(identity=self.id, expires_delta=expire_delta)
         return token
 
     @classmethod
     def authenticate(cls, email, password):
-        user = cls.query.filter(cls.email == email).one()
-        if not bcrypt.verify(password, user.password):
-            raise Exception('No user with this password')
-        return user
+        try:
+            user = session.query(cls).filter(cls.email == email).first()
+            if not user or not checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                raise Unauthorized('Invalid email or password')
+            return user
+        except NoResultFound:
+            raise Unauthorized('Invalid email or password')
 
 
 class Products(Base):
-    """Товары"""
-
+    """
+    Товары
+    """
     __tablename__ = 'products'
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -56,31 +64,32 @@ class Products(Base):
 
 
 class Orders(Base):
-    """Заказы"""
-
+    """
+    Заказы
+    """
     __tablename__ = 'orders'
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     order_date: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    # order_status: Mapped[Enum] = mapped_column(Enum(OrderStatus)) TODO Сделать enum
 
 
 class Payments(Base):
-    """Платежи"""
-
+    """
+    Платежи
+    """
     __tablename__ = 'payments'
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     order_id: Mapped[int] = mapped_column(Integer, ForeignKey('orders.id'))
     payment_date: Mapped[DateTime | None] = mapped_column(DateTime(timezone=True), server_default=func.now())
     payment_amount: Mapped[int | None] = mapped_column(Integer)
-    # payment_status: Mapped[Enum] = mapped_column(Enum()) TODO Сделать enum
 
 
 class OrderHistory(Base):
-    """История заказов"""
-
+    """
+    История заказов
+    """
     __tablename__ = 'order_history'
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
